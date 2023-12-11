8f0f8a04fb7604bbec823c7431f74287fde90f54
herohua1026 <89980991+herohua1026@users.noreply.github.com>
2023-12-11 12:49:34 +1000

    Add files via upload

---
+++ models/TULER/config.py
@@ -0,0 +1,23 @@
+learning_rate = 0.00095  # 0.00095
+train_iters = 100  #
+dispaly_step = 1000
+batch_size = 1  #
+User_List = list()  #
+
+lambda_loss_amount = 0.001
+n_input = 250  # embedding size
+n_hidden = 300  # hidden size
+w_2 = 28
+n_classes = 112  # clss number
+n_steps = [3]
+
+
+n_traj = 8200
+
+data_input_path = '../../data/segmented/'
+data_input = 'Gowalla_day_1seg.dat'
+
+vec_input_path = '../../data/segmented/'
+vec_input = 'Gowalla_day_1seg_cbow_8500.dat'
+
+result_out_path = './output/'
